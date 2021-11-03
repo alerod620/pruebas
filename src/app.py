e045af9b1928df574a625617530d023bea777ad7
helmut <efraalv@gmail.com>
2021-11-03 16:28:07 -0600

    .

--- src/app.py
+++ src/app.py
@@ -88,4 +88,4 @@ def asistencia_carnet(id):
         return response   
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=3500, debug=True)        
+    app.run(host='0.0.0.0',port=3500, debug=False)        
